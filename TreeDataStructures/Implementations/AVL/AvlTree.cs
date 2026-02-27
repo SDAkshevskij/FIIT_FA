e80460e9a0aaedfbf9bacc225250528a187a4788
User <cat28206@mail.ru>
2026-02-27 19:14:00 +0300

    feat(avl): implement avl

--- TreeDataStructures/Implementations/AVL/AvlTree.cs
+++ TreeDataStructures/Implementations/AVL/AvlTree.cs
@@ -1,4 +1,5 @@
-﻿using TreeDataStructures.Core;
+﻿using System.Runtime.CompilerServices;
+using TreeDataStructures.Core;
 
 namespace TreeDataStructures.Implementations.AVL;
 
@@ -10,7 +11,64 @@ public class AvlTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, AvlNode<
     
     protected override void OnNodeAdded(AvlNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        AvlNode<TKey, TValue>? curNode = newNode.Parent;
+
+        while(curNode != null)
+        {
+            MakeBalance(curNode);
+            curNode = curNode.Parent;
+        }
+    }
+    protected override void OnNodeRemoved(AvlNode<TKey, TValue>? parent, AvlNode<TKey, TValue>? child)
+    {
+        AvlNode<TKey, TValue>? curNode = parent ?? child;
+        while (curNode != null)
+        {
+            MakeBalance(curNode);
+            curNode = curNode.Parent;
+        }
+    }
+
+    
+    private void MakeBalance(AvlNode<TKey, TValue> node)
+    {
+        int disbalance = GetDisbalance(node);
+        if (Math.Abs(disbalance) > 1)
+        {
+            if (disbalance == 2 && GetDisbalance(node!.Left) == 1)
+            {
+                RotateRight(node);
+            }
+            else if (disbalance == -2 && GetDisbalance(node.Right) == -1)
+            {
+                RotateLeft(node);
+            }
+            else if (disbalance == 2 && GetDisbalance(node!.Left) == -1)
+            {
+                RotateBigRight(node);
+            }
+            else if (disbalance == -2 && GetDisbalance(node!.Right) == 1)
+            {
+                RotateBigLeft(node);
+            }
+            else
+            {
+                throw new Exception("Unknow AVL balance situation");
+            }
+        }
+        node.Height = CountHeight(node);
+    }
+    private int GetDisbalance(AvlNode<TKey, TValue> node)
+    {
+        int leftH = node.Left == null ? 0 : node.Left.Height;
+        int rightH = node.Right == null ? 0 : node.Right.Height;
+        return leftH - rightH;
+    }
+    private int CountHeight(AvlNode<TKey, TValue> node)
+    {
+        int leftH = node.Left == null ? 0 : node.Left.Height;
+        int rightH = node.Right == null ? 0 : node.Right.Height;
+        return Math.Max(leftH, rightH) + 1;
     }
 
     
